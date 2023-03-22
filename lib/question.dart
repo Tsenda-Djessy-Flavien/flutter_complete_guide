6f306398b9bcef760de3a4e36cb26b97044af3c3
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-03-22 12:59:34 +0000

    First Styling & Layouting Steps

--- lib/question.dart
+++ lib/question.dart
@@ -12,6 +12,16 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: const TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
